6f45df0d27992f728faa4a8c6fd5a9f983536b20
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-02-28 02:11:11 +0600

    return

---
+++ return.js
@@ -0,0 +1,7 @@
+// return 
+function fruitPrice (apple, orange, banana) {
+    var totalPrice = apple + orange + banana;
+    return totalPrice; 
+}
+var price = fruitPrice(300, 500, 200);
+console.log(price);
